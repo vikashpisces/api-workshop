e383723b236054a7ae9a3472f23f03adb958d6f5
vikash_gupta <vikash.k@appnomic.com>
2023-09-03 12:42:14 +0530

    adds mongo db, models and api route talks to DB now

--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router()
 
-const products = require('../products.json')
+const Product = require('../models/product.js')
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const products = await Product.find()
   res.json(products)
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params
-  const product = products.find((product) => product.id === +id)
+  const product = await Product.findById(id)
   if (!product) {
     return res.status(404).send('Product not found')
   }
   res.json(product)
 })
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const product = req.body
-  product.id = products.length + 1
-  products.push(product)
-  res.send('Product created')
+  const newProduct = await Product.create(product)
+  res.send(newProduct)
 })
 
 router.put("/:id", (req, res) => {
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
 const PORT = process.env.PORT || 8080
 const productsRouter = require('./routes/product.js')
 
+async function connectMongo() { 
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING) 
+  } catch (err) { 
+    if (err) {
+      console.log("Error in connecting to mongoose. Details: ", err)
+    } else {
+      console.log('Connected to the database')
+    }
+  }
+}
+
+connectMongo()
 app.use(express.json())
 
 const logger = (req, res, next) => {
